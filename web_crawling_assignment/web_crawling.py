a92c4747fab5696d4c69f0011588328f0ad4e7d8
nag2mani <nag2mani@gmail.com>
2024-02-12 16:56:51 +0530

    final submit

--- web_crawling_assignment/web_crawling.py
+++ web_crawling_assignment/web_crawling.py
@@ -1,7 +1,10 @@
 import requests
 
 def title(url):
+    # This function will print the title of the webpage.
+
     response = requests.get(url)
+    #This raw_content will code all the texts with script from webpages.
     raw_content = response.text
     title_start = raw_content.find("<title>") + len("<title>")
     title_end = raw_content.find("</title>")
@@ -9,7 +12,10 @@ def title(url):
     print(title)
 
 
+
 def body(url):
+    # This function will print the content of the body.
+
     response = requests.get(url)
     html_content = response.text
 
@@ -29,16 +35,19 @@ def body(url):
                 start_index = html_content.find(">", end_index) + 1
             else:
                 break
-    
+    # This will remove the unwanted things from the texts and add to the result.
     result = ''
     for i in texts:
         if (i[0] not in ['.', ',','', ' ', '(', ')', '[', ']', '{', '}', '@', "'", '&', '#', '^']):
-            if i[0:3] not in ['-->', 'htt']:
+            if i[0:3] not in ['-->', 'htt', 'jQu']:
                 result = result + i + ' '
     print(result)
 
 
+
+
 def links(url):
+    # This function will print the links in the webpage.
     response = requests.get(url)
     raw_content = response.text
 
@@ -46,14 +55,15 @@ def links(url):
 
     link = []
     while len(body_raw_content) > 1:
-        s = body_raw_content.find("http")
+        s = body_raw_content.find("https://")
         e = body_raw_content[s:].find('"')
         link.append(body_raw_content[s :s + e])
         body_raw_content = body_raw_content[s + e+1:]
+    
+    for i in link:
+        print(i)
 
-    for j in link[:len(link)-7]:
-        if (j[0:6] not in ['https%', 'https&']) and (j[0:5] not in ['http%']):
-            print(j)
+
 
 
 def main():
